36c413ae130c6290749dde795ac11c1cd9f70d3c
ecleaver <ecleaver@live.com>
2026-05-13 16:54:27 -0400

    Update godot

--- foundations/Scenes/Ocean/Ocean.gd
+++ foundations/Scenes/Ocean/Ocean.gd
@@ -2,11 +2,21 @@ extends Node2D
 
 @onready var plane: AnimatedSprite2D = $PlaneAnimated2d
 @onready var hilcopper: AnimatedSprite2D = $HilcopperAnimated2d
-@onready var radius = 120
-@onready var angle = 0.0
-@onready var speed = 1.5
+@onready var radius = 50
+@onready var angle = 0
+@onready var speed = 0.5
 @onready var pos_x = 0
 @onready var pos_y = 0
+@onready var x = 50
+@onready var y = 50
+func createCircle(sprite : AnimatedSprite2D, speed, angle, radius, pos_x, pos_y, x, y):
+		
+	angle += speed * get_process_delta_time()
+	pos_x = cos(angle)
+	pos_y = sin(angle)
+	sprite.position.x = radius * pos_x + x
+	sprite.position.y = radius * pos_y + y
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -29,13 +39,18 @@ func _process(delta: float) -> void:
 	elif plane.position.y >= 620:
 		plane.position.y -= 120 * delta
 		plane.rotate(180 * delta)
+		
+	"
+	 	Trying to replace the logic of the circle with a function.  
+	  createCircle(hilcopper, speed, angle, radius, pos_x, pos_y, x, y)
+	"
 	
 	angle += speed * get_process_delta_time()
 	pos_x = cos(angle)
 	pos_y = sin(angle)
-	hilcopper.position.x = radius * pos_x + 150
-	hilcopper.position.y = radius * pos_y + 150
-
+	hilcopper.position.x = radius * pos_x + x
+	hilcopper.position.y = radius * pos_y + y
+	
 	if Input.is_action_pressed("ui_left"):
 		plane.rotate(-1.5 * delta)
 	elif Input.is_action_pressed("ui_right"):
