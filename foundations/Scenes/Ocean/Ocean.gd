1fc8ac5c288780cd6d81c17962092582aaebdd5c
ecleaver <ecleaver@live.com>
2026-05-13 09:04:10 -0400

    Updated code for godot script

--- foundations/Scenes/Ocean/Ocean.gd
+++ foundations/Scenes/Ocean/Ocean.gd
@@ -2,8 +2,11 @@ extends Node2D
 
 @onready var plane: AnimatedSprite2D = $PlaneAnimated2d
 @onready var hilcopper: AnimatedSprite2D = $HilcopperAnimated2d
-@onready var plane_direction = "east"
-@onready var hilcopper_direction = "south"
+@onready var radius = 120
+@onready var angle = 0.0
+@onready var speed = 1.5
+@onready var pos_x = 0
+@onready var pos_y = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,28 +15,30 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if (plane_direction == "east"):
-		if (plane.position.x <= 1100):
-			plane.position.x += 100.0 * delta
-		else: 
-			plane.flip_h = true
-			plane_direction = "west"
-	if (plane_direction == "west"):
-		if (plane.position.x >=  50):
-			plane.position.x -= 100.0 * delta
-		else: 
-			plane_direction = "east"
-			plane.flip_h = false
-			
-	if (hilcopper_direction == "south"):
-		if (hilcopper.position.y <= 600):
-			hilcopper.position.y += 50.0 * delta
-		else: 
-			hilcopper_direction = "north"
-	if (hilcopper_direction == "north"):
-		if (hilcopper.position.y >=  50):
-			hilcopper.position.y -= 50.0 * delta
-		else: 
-			hilcopper_direction = "south"
+	if (plane.position.x > 30 and plane.position.x < 1120 and plane.position.y > 30 and plane.position.y < 620):
+		plane.move_local_x(-100 * delta)
+	elif plane.position.x <= 30 :
+		plane.position.x += 120 * delta
+		plane.rotate(180 * delta)
+	elif plane.position.x >= 1120:
+		plane.position.x -= 120 * delta
+		plane.rotate(180 * delta)
+	elif plane.position.y <= 30:
+		plane.position.y += 120 * delta
+		plane.rotate(180 * delta)
+	elif plane.position.y >= 620:
+		plane.position.y -= 120 * delta
+		plane.rotate(180 * delta)
+	
+	angle += speed * get_process_delta_time()
+	pos_x = cos(angle)
+	pos_y = sin(angle)
+	hilcopper.position.x = radius * pos_x + 150
+	hilcopper.position.y = radius * pos_y + 150
+
+	if Input.is_action_pressed("ui_left"):
+		plane.rotate(-1.5 * delta)
+	elif Input.is_action_pressed("ui_right"):
+		plane.rotate(1.5 * delta)
 
 	pass
