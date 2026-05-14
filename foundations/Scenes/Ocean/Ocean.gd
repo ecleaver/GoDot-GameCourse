96aac20ad51e0dbfcc745c29c73e914dc376187f
ecleaver <ecleaver@live.com>
2026-05-13 20:27:20 -0400

    Foundations
    Found limitations of the godot, it dose not allow for the delta to pass to the function

--- foundations/Scenes/Ocean/Ocean.gd
+++ foundations/Scenes/Ocean/Ocean.gd
@@ -2,21 +2,17 @@ extends Node2D
 
 @onready var plane: AnimatedSprite2D = $PlaneAnimated2d
 @onready var hilcopper: AnimatedSprite2D = $HilcopperAnimated2d
-@onready var radius = 50
+@onready var radius = 100
 @onready var angle = 0
 @onready var speed = 0.5
-@onready var pos_x = 0
-@onready var pos_y = 0
-@onready var x = 50
-@onready var y = 50
-func createCircle(sprite : AnimatedSprite2D, speed, angle, radius, pos_x, pos_y, x, y):
-		
-	angle += speed * get_process_delta_time()
-	pos_x = cos(angle)
-	pos_y = sin(angle)
-	sprite.position.x = radius * pos_x + x
-	sprite.position.y = radius * pos_y + y
-	
+@onready var pos_x = 500
+@onready var pos_y = 300
+@onready var positions = []
+
+func createCircle (sprit : AnimatedSprite2D, radius, angle, pos_x, pos_y):
+	var positions = [radius * cos(angle) + pos_x, radius * sin(angle) + pos_y]
+	sprit.position.x = positions[0]
+	sprit.position.y = positions[1]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -40,16 +36,13 @@ func _process(delta: float) -> void:
 		plane.position.y -= 120 * delta
 		plane.rotate(180 * delta)
 		
-	"
-	 	Trying to replace the logic of the circle with a function.  
-	  createCircle(hilcopper, speed, angle, radius, pos_x, pos_y, x, y)
-	"
-	
+
 	angle += speed * get_process_delta_time()
-	pos_x = cos(angle)
-	pos_y = sin(angle)
-	hilcopper.position.x = radius * pos_x + x
-	hilcopper.position.y = radius * pos_y + y
+	createCircle(hilcopper, radius, angle, pos_x, pos_y)
+	
+	
+	
+	
 	
 	if Input.is_action_pressed("ui_left"):
 		plane.rotate(-1.5 * delta)
