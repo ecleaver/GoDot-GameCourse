a371a667208ad37e68cf1c21b17fcdef3a4d823a
ecleaver <ecleaver@live.com>
2026-05-15 06:18:41 -0400

    Changed to move with mouse

--- foundations/Scenes/Ocean/Ocean.gd
+++ foundations/Scenes/Ocean/Ocean.gd
@@ -5,7 +5,7 @@ extends Node2D
 @onready var eatingsound: AudioStreamPlayer = $Eatingsound
 @onready var radius = 100
 @onready var angle = 0
-@onready var speed = 0.5
+@onready var speed = 1
 @onready var pos_x = 500
 @onready var pos_y = 300
 @onready var positions = []
@@ -47,7 +47,18 @@ func planeInputs(sprit : AnimatedSprite2D, plane_rotate):
 		print("space") 
 		eatingsound.play()
 		#plane.global_position = Vector2(pos_x, pos_y)
-	
+
+func moveToMouse(sprit1 : AnimatedSprite2D, move, speed):
+	sprit1.look_at(get_global_mouse_position())
+	sprit1.move_local_x(-(speed * move))
+
+func moveTo(sprit1 : AnimatedSprite2D, sprit2 : AnimatedSprite2D, move, speed):
+	var direction: Vector2 = sprit1.position.direction_to(sprit2.position)
+	sprit1.look_at(sprit2.position)
+	sprit1.translate(direction * speed * move)
+
+	pass
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -61,15 +72,21 @@ func _process(delta: float) -> void:
 	move = 100 * delta
 	rotate = 180 * delta
 	plane_rotate = 1.5 * delta
+	moveToMouse(plane, move, speed)
+	#moveTo(plane, hilcopper, move, speed)
+	#hilcopper.position.y += 20 * delta
+	#gets the planes current potions 
 	
 	#put the code for moving the plane into a 
 	movePlane(plane, move, min_xy, max_x, max_y, rotate)
 	#Delta needs to be outside of the fuctnion and passed in
 	
 	angle += speed * get_process_delta_time()
+	# a function to make a object move in a circle
 	createCircle(hilcopper, radius, angle, pos_x, pos_y)
 	
-	planeInputs(plane, plane_rotate)
+	#a object for getting inputs 
+	#planeInputs(plane, plane_rotate)
 		
 	
 	
