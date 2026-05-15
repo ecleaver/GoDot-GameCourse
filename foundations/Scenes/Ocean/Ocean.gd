bc7fc7c58264e1c785950120a5992a26e9ba9259
ecleaver <ecleaver@live.com>
2026-05-15 05:48:59 -0400

    Added to functions

--- foundations/Scenes/Ocean/Ocean.gd
+++ foundations/Scenes/Ocean/Ocean.gd
@@ -20,7 +20,34 @@ func createCircle (sprit : AnimatedSprite2D, radius, angle, pos_x, pos_y):
 	var positions = [radius * cos(angle) + pos_x, radius * sin(angle) + pos_y]
 	sprit.position.x = positions[0]
 	sprit.position.y = positions[1]
+	
+func movePlane(sprit : AnimatedSprite2D, move, min_xy, max_x, max_y, rotate):
+	if (sprit.global_position.x > min_xy and sprit.global_position.x < max_x and sprit.global_position.y > min_xy and sprit.global_position.y < max_y):
+		#setting move_local_x to false removes the gobal scaling affects 
+		sprit.move_local_x(-move)
+	elif plane.global_position.x <= min_xy :
+		sprit.global_position.x += move
+	elif plane.global_position.x >= max_x:
+		sprit.global_position.x -= move
+	elif plane.global_position.y <= min_xy:
+		sprit.global_position.y += move
+	elif plane.global_position.y >= max_y:
+		sprit.global_position.y -= move
+		
+	if (sprit.global_position.x <= min_xy or sprit.global_position.x >= max_x or sprit.global_position.y <= min_xy or sprit.global_position.y >= max_y):
+		sprit.rotate(rotate)
 
+func planeInputs(sprit : AnimatedSprite2D, plane_rotate):
+	if Input.is_action_pressed("ui_left"):
+		sprit.rotate(-plane_rotate)
+	elif Input.is_action_pressed("ui_right"):
+		sprit.rotate(plane_rotate)
+		
+	if Input.is_action_just_pressed("ui_accept"):
+		print("space") 
+		eatingsound.play()
+		#plane.global_position = Vector2(pos_x, pos_y)
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -34,35 +61,16 @@ func _process(delta: float) -> void:
 	move = 100 * delta
 	rotate = 180 * delta
 	plane_rotate = 1.5 * delta
-	if (plane.global_position.x > min_xy and plane.global_position.x < max_x and plane.global_position.y > min_xy and plane.global_position.y < max_y):
-		#setting move_local_x to false removes the gobal scaling affects 
-		plane.move_local_x(-100 * delta)
-	elif plane.global_position.x <= min_xy :
-		plane.global_position.x += move
-	elif plane.global_position.x >= max_x:
-		plane.global_position.x -= move
-	elif plane.global_position.y <= min_xy:
-		plane.global_position.y += move
-	elif plane.global_position.y >= max_y:
-		plane.global_position.y -= move
-
-	if (plane.global_position.x <= min_xy or plane.global_position.x >= max_x or plane.global_position.y <= min_xy or plane.global_position.y >= max_y):
-		plane.rotate(rotate)
 	
-	
+	#put the code for moving the plane into a 
+	movePlane(plane, move, min_xy, max_x, max_y, rotate)
 	#Delta needs to be outside of the fuctnion and passed in
 	
 	angle += speed * get_process_delta_time()
 	createCircle(hilcopper, radius, angle, pos_x, pos_y)
 	
-	if Input.is_action_pressed("ui_left"):
-		plane.rotate(-plane_rotate)
-	elif Input.is_action_pressed("ui_right"):
-		plane.rotate(plane_rotate)
+	planeInputs(plane, plane_rotate)
 		
-	if Input.is_action_just_pressed("ui_accept"):
-		print("space") 
-		#eatingsound.play()
-		plane.global_position = Vector2(pos_x, pos_y)
+	
 	
 	pass
