b7f16e5e62fb7f35fc1fe12f2da41dec9a2ed7ce
ecleaver <ecleaver@live.com>
2026-05-21 08:54:02 -0400

    Fix spawning bug
    Fix a bug were they were dpawning

--- Droper-Game/Scripts/Bottle.gd
+++ Droper-Game/Scripts/Bottle.gd
@@ -3,12 +3,17 @@ extends Area2D
 @onready var bottles: Bottles = %Bottles
 
 signal game_over
-
-
+const _MAX_SPAWN : float = 100
+var _random_size : float = randf_range(.2, .4)
+var _view_range : Rect2 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+
+
+func set_range(p_view_range) ->void:
+	_view_range = p_view_range
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -22,7 +27,14 @@ func check_game_over() -> void:
 		queue_free()
 	pass
 
-
+func randomized_settings() ->void:
+	
+	scale = Vector2(_random_size, _random_size)
+	
+	var x_start : float = _view_range.position.x + _MAX_SPAWN
+	var x_end : float = _view_range.end.x + _MAX_SPAWN
+	var rand_postion_x : float = randf_range(x_start, x_end)
+	position = Vector2(rand_postion_x, _MAX_SPAWN)
 
 func _physics_process(delta: float) -> void:
 	position.y += 100 * delta
--- Droper-Game/Scripts/Game.gd
+++ Droper-Game/Scripts/Game.gd
@@ -3,6 +3,9 @@ extends Node2D
 const BOTTLE = preload("uid://dxywco7xxb2cr")
 @onready var spawn_timer: Timer = $Spawn
 @onready var  spawn_time: float = 2.0
+@onready var view_range : Rect2 = get_viewport_rect()
+
+
 
 func _physics_process(delta: float) -> void:
 	
@@ -14,13 +17,13 @@ func _ready() -> void:
 	
 func spawn_bottle() -> void:
 	var new_bottle : Bottle = BOTTLE.instantiate()
+	new_bottle.set_range(view_range)
+	new_bottle.randomized_settings()
 	new_bottle.game_over.connect(_on_bottle_game_over)
-	var random_size : float = randf_range(.2, .4)
-	new_bottle.scale = Vector2(random_size, random_size)
-	var rand_postion_x : float = randf_range(50, 1000)
-	new_bottle.position = Vector2(rand_postion_x, 50)
 	add_child(new_bottle)
 	pass
+
+
 
 func _on_bottle_game_over() ->void:
 	print("game over")
