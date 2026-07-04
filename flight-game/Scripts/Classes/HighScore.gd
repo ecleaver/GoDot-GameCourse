6b988b201fa8bdaee5bed97d6bef583243d572b7
ecleaver <ecleaver@live.com>
2026-07-04 13:08:34 -0400

    Rigidbody-Game

--- flight-game/Scripts/Classes/HighScore.gd
+++
@@ -1,4 +0,0 @@
-extends Resource
-class_name HighScore
-
-@export var high_score : int = 0
--- flight-game/Scripts/Globals/SignalHub.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-signal on_bird_died
-signal on_point_scored
-
-func emit_on_death() ->void:
-	on_bird_died.emit()
-
-func emit_on_point_scored() ->void:
-	on_point_scored.emit()
